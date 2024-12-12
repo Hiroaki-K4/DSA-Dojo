fc2a8e7302a88e3329beb8b72c090ae6fd817106
Hiroaki-K4 <49no19@gmail.com>
2024-12-12 10:16:02 -0800

    Implement hash table

--- data_structures/hash_table.py
+++ data_structures/hash_table.py
@@ -6,29 +6,137 @@ class Node:
 
 
 class HashTable:
-    def __init__(self, capacity):
-        self.table = [None] * capacity
+    def __init__(self, capacity) -> None:
+        self.capacity = capacity
+        self.table = [None] * self.capacity
 
-    def insert(self, key, value):
-        print(hash(key))
-        idx = hash(key)
-        # TODO: Implement insert function
+    def insert(self, key, value) -> None:
+        idx = hash(key) % self.capacity
         if self.table[idx]:
-            pass
+            node = self.table[idx]
+            while node.next:
+                if node.key == key:
+                    node.value = value
+                    return
+                node = node.next
+            node.next = Node(key, value)
         else:
-            pass
+            self.table[idx] = Node(key, value)
 
-    def pop(self, key):
-        pass
+    def pop(self, key) -> None:
+        idx = hash(key) % self.capacity
+        if self.table[idx]:
+            node = self.table[idx]
+            if node.key == key:
+                if not node.next:
+                    self.table[idx] = None
+                    return
+                else:
+                    self.table[idx] = node.next
+                    return
+            while node.next:
+                if node.next.key == key:
+                    node.next = node.next.next
+                    return
+                node = node.next
 
-    def find(self, key):
-        pass
+    def get(self, key):
+        idx = hash(key) % self.capacity
+        if self.table[idx]:
+            node = self.table[idx]
+            while node:
+                if node.key == key:
+                    return node.value
+                node = node.next
+        return None
+
+    def find(self, key) -> bool:
+        idx = hash(key) % self.capacity
+        if self.table[idx]:
+            node = self.table[idx]
+            while node:
+                if key == node.key:
+                    return True
+                node = node.next
+        return False
 
 
 if __name__ == "__main__":
-    capacity = 1024
+    capacity = 3
     hash_table = HashTable(capacity)
-    hash_table.insert("Apple", 0)
-    hash_table.insert("Banana", 1)
-    hash_table.insert("Orange", 2)
-    hash_table.insert(1, 2)
+
+    test_case_1 = ["Apple", 0]
+    test_case_2 = ["Banana", 1]
+    test_case_3 = ["Orange", 2]
+    test_case_4 = ["Lemon", 3]
+    test_case_5 = ["Melon", 4]
+
+    hash_table.insert(test_case_1[0], test_case_1[1])
+    hash_table.insert(test_case_2[0], test_case_2[1])
+    hash_table.insert(test_case_3[0], test_case_3[1])
+    hash_table.insert(test_case_4[0], test_case_4[1])
+
+    print("TEST: insert and get function")
+    print("Key: {0}, Value: {1}".format(test_case_1[0], hash_table.get(test_case_1[0])))
+    print("Key: {0}, Value: {1}".format(test_case_2[0], hash_table.get(test_case_2[0])))
+    print("Key: {0}, Value: {1}".format(test_case_3[0], hash_table.get(test_case_3[0])))
+    print("Key: {0}, Value: {1}".format(test_case_4[0], hash_table.get(test_case_4[0])))
+    print()
+
+    print("TEST: find function")
+    print(
+        "Is there {0} in hash table? -> {1}".format(
+            test_case_1[0], hash_table.find(test_case_1[0])
+        )
+    )
+    print(
+        "Is there {0} in hash table? -> {1}".format(
+            test_case_2[0], hash_table.find(test_case_2[0])
+        )
+    )
+    print(
+        "Is there {0} in hash table? -> {1}".format(
+            test_case_3[0], hash_table.find(test_case_3[0])
+        )
+    )
+    print(
+        "Is there {0} in hash table? -> {1}".format(
+            test_case_4[0], hash_table.find(test_case_4[0])
+        )
+    )
+    print(
+        "Is there {0} in hash table? -> {1}".format(
+            test_case_5[0], hash_table.find(test_case_5[0])
+        )
+    )
+    print()
+
+    hash_table.pop(test_case_1[0])
+    hash_table.pop(test_case_3[0])
+    hash_table.pop(test_case_5[0])
+    print("TEST: pop function")
+    print(
+        "Is there {0} in hash table? -> {1}".format(
+            test_case_1[0], hash_table.find(test_case_1[0])
+        )
+    )
+    print(
+        "Is there {0} in hash table? -> {1}".format(
+            test_case_2[0], hash_table.find(test_case_2[0])
+        )
+    )
+    print(
+        "Is there {0} in hash table? -> {1}".format(
+            test_case_3[0], hash_table.find(test_case_3[0])
+        )
+    )
+    print(
+        "Is there {0} in hash table? -> {1}".format(
+            test_case_4[0], hash_table.find(test_case_4[0])
+        )
+    )
+    print(
+        "Is there {0} in hash table? -> {1}".format(
+            test_case_5[0], hash_table.find(test_case_5[0])
+        )
+    )
