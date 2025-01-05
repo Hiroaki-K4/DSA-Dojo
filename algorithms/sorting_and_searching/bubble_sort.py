7c5ba9886c0bb79792c03a392a729b49342dc551
Hiroaki-K4 <49no19@gmail.com>
2025-01-04 19:25:24 -0800

    Add bubble sort

---
+++ algorithms/sorting_and_searching/bubble_sort.py
@@ -0,0 +1,16 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        if not swapped:
+            break
+    return arr
+
+
+if __name__ == "__main__":
+    array = [5, 3, 8, 6, 2]
+    print("Sorted array: ", bubble_sort(array))
